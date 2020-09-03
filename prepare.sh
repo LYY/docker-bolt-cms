4f4592024f0757c2933ef3a592200a03bc5f28fa
LYY <shinxjl+github@gmail.com>
2020-09-03 21:30:36 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -7,8 +7,8 @@ D_PST_THEME=$D_PUBLIC/pst_theme
 F_THEME_PROCED=$D_THEME/processed.pt
 if [ -d "$D_PST_THEME" -a -d "$D_THEME" -a ! -d "$D_BAK_THEME" ]; then
 	echo "run D_PST_THEME"
-	mv $D_THEME $D_BAK_THEME
-	ln -s $D_PST_THEME $D_THEME
+	mv "$D_THEME" "$D_BAK_THEME"
+	ln -s "$D_PST_THEME" "$D_THEME"
     if [ ! -f "$F_THEME_PROCED" ]; then
 		echo "run F_THEME_PROCED"
     	cp -r $D_BAK_THEME/* $D_PST_THEME/
